8b46bfb912ed91e3767d4c2bc5dbd9648ab7506b
Owen <ocole161@gmail.com>
2023-03-13 21:06:22 -0600

    React Bootstrap Install

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,10 @@
+function NavBar() {
+    return(
+        <nav>
+            <a href="/login">login</a><> </>
+            <a href="/signup">Signup</a>
+        </nav>
+    )
+}
+
+export default NavBar
