9fbcda30319cb8d777a9bd15fb4b9e4755df262b
Owen <ocole161@gmail.com>
2023-03-17 23:03:21 -0600

    SpecialView build, last commit before trying reactjs-popup

--- client/src/components/SpecialView.js
+++ client/src/components/SpecialView.js
@@ -7,15 +7,30 @@ import Image from 'react-bootstrap/Image'
 function SpecialView() {
     const specials = useSelector((state) => state.specials);
     const { id } = useParams()
-
     const special = specials.find((special) => special.id === parseInt(id))
-    // console.log(special)
-    console.log(specials[0])
+    const startTime = new Date(special.start_time);
+    const endTime = new Date(special.end_time);
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true, timeZone: 'UTC'};
+    const startTimeString = startTime.toLocaleTimeString('eng-US', options);
+    const endTimeString = endTime.toLocaleTimeString('eng-US', options)
 
     return (
         <>
         <h1 className="special-view-title">{special.location_name}</h1>
         <Image fluid thumbnail="true" rounded="true" src={special.location_image} alt={special.location_name} />
+        <h3>Happy Hour: {startTimeString} - {endTimeString}</h3>
+        <h3>Days: 
+            {special.monday ? " Monday" : null}
+            {special.tuesday ? " Tuesday" : null}
+            {special.wednesday ? " Wednesday": null}
+            {special.thursday ? " Thursday" : null}
+            {special.friday ? " Friday" : null}
+            {special.saturday ? " Saturday" : null}
+            {special.sunday ? " Sunday" : null}
+        </h3>
+        <p>{special.location_address}</p>
+        <p>Specials: {special.hh_special_text}</p>
+        <a href={special.location_url}>Website</a>
         </>
     )
 }
